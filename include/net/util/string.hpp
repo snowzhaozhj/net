e9072a332aeb9fc8ddbab1b373a2fb8e1e556a5f
zhj12138 <1550223143@qq.com>
2022-03-29 11:38:00 +0800

    fix: --bug=缺少inline声明

--- include/net/util/string.hpp
+++ include/net/util/string.hpp
@@ -5,7 +5,7 @@
 
 namespace net {
 
-bool HasPrefix(std::string_view s, std::string_view prefix) {
+inline bool HasPrefix(std::string_view s, std::string_view prefix) {
   if (prefix.size() > s.size()) return false;
   return std::equal(prefix.begin(), prefix.end(),
                     s.begin());
@@ -14,7 +14,7 @@ bool HasPrefix(std::string_view s, std::string_view prefix) {
 /// @brief 如果s包含前缀prefix，则将去除前缀后的s写入result\n
 /// @brief 如果s不包含前缀prefix，则不对result进行修改
 /// @return 返回true表示s包含前缀prefix，返回false表示s不包含前缀prefix
-bool RemovePrefix(std::string &result, std::string_view s, std::string_view prefix) {
+inline bool RemovePrefix(std::string &result, std::string_view s, std::string_view prefix) {
   if (!HasPrefix(s, prefix)) return false;
   result = s.substr(prefix.size());
   return true;
